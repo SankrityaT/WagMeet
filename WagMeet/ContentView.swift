4aa7c1902e1608b8f5ce67808cca6eaeda3e7d04
SankrityaT <sthaku20@asu.edu>
2024-09-09 00:01:04 -0700

    Initial Commit

---
+++ WagMeet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WagMeet
+//
+//  Created by Sankritya Thakur on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WagMeet/WagMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WagMeetApp.swift
+//  WagMeet
+//
+//  Created by Sankritya Thakur on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WagMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
